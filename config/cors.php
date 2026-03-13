929ff7991754d14b446fa10208841a3fa28bab60
Ni Putu Lidya Pramesty <lidyapramesti46@gmail.com>
2026-03-13 09:00:56 +0700

    feat: Add initial CORS configuration file.

--- config/cors.php
+++ config/cors.php
@@ -12,8 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'https://jelita-bps.vercel.app,http://localhost:3000'))),
-'allowed_origins' => ['*'],
+    'allowed_origins' => array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'https://jelita-bps.vercel.app,http://localhost:3000'))),
 
     'allowed_origins_patterns' => [],
 
